21f80beda9eae2e9759f00c4b25576c691404cf4
kimsoohyun <shkimm9608@naver.com>
2024-05-26 11:47:09 +0900

    즉시 로딩과 지연 로딩

--- JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,10 @@
 package hellojpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -18,16 +17,34 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(team);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); //Proxy
-            Hibernate.initialize(refMember); //강제초기화
+//            Member m = em.find(Member.class, member1.getId());
+
+            List<Member> members = em.createQuery("select m from Member m Join fetch m.team", Member.class)
+                    .getResultList();
+
+            //SQL: select * from Member
+            //SQL: select * from Team where TEAM_ID = xxx
 
 
             tx.commit();
