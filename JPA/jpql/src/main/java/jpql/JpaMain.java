6d5b3369fb776fb422311afb692e6d7b36daa879
kimsoohyun <shkimm9608@naver.com>
2024-06-08 10:41:57 +0900

    경로 표현식

--- JPA/jpql/src/main/java/jpql/JpaMain.java
+++ JPA/jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -17,26 +18,29 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
+
             Member member2 = new Member();
             member2.setUsername("관리자1");
+            member2.setTeam(team);
             em.persist(member2);
 
 
             em.flush();
             em.clear();
 
-            String query = "select group_concat(m.username) from Member m";
+            String query = "select m from Team t join t.members m";
 
-            List<String> result = em.createQuery(query, String.class)
+            List<Collection> result = em.createQuery(query, Collection.class)
                     .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
-
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
