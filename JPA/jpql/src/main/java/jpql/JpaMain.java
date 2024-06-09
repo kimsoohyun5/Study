4e084dbaceda97ab20bb3d54f7af3e8ec4cd3e6b
kimsoohyun <shkimm9608@naver.com>
2024-06-09 09:32:06 +0900

    페치 조인 2 - 한계

--- JPA/jpql/src/main/java/jpql/JpaMain.java
+++ JPA/jpql/src/main/java/jpql/JpaMain.java
@@ -44,9 +44,11 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t from Team t join fetch t.members";
+            String query = "select t from Team t";
 
             List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
 
             System.out.println("result = " + result.size());
